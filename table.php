182cb794b8a7c7f1fa2d8452bfac28983c76837c
Ndugwa Adam <96383198+ndugwaadam@users.noreply.github.com>
2023-07-19 15:16:01 +0300

    Updated  table.php by ndugwa adam

--- table.php
+++ table.php
@@ -30,39 +30,23 @@ body,td,th {
         
 			<thead>
              
-           <th bgcolor="#FFFFFF" bordercolor="#000000" width="122">Sur Name</th>
-				<th bgcolor="#FFFFFF" width="100">Given Name</th>
-                <th bgcolor="#FFFFFF" width="78">Gender/Sex</th>
-                <th bgcolor="#FFFFFF" width="101">Date Of Birth</th>
-                <th bgcolor="#FFFFFF" width="119">Address</th>
-                <th bgcolor="#FFFFFF" width="119">Telephone</th>
-                <th bgcolor="#FFFFFF" width="132">Email Address</th>
-                <th bgcolor="#FFFFFF" width="110">Marital Status</th>
-                <th bgcolor="#FFFFFF" width="93">Date Of Registration</th>
-                
-				
+           <th bgcolor="#FFFFFF" bordercolor="#000000" width="10">Feedback Id</th>
+				<th bgcolor="#FFFFFF" width="10">rating</th>
+                <th bgcolor="#FFFFFF" width="78">message</th>
+               
 			
 			<tbody>
 				<?php
-					include('conn.php');
-					$query=mysqli_query($conn,"select * from `feedback`");
+					include('con1.php');
+					$query=mysqli_query($bd,"select * from `feedback`");
 					while($row=mysqli_fetch_array($query)){
 						?>
 						<tr>
 			
-                          <td bgcolor="#EFEFEF" height="46"><?php echo $row['sname']; ?></td>
-							<td bgcolor="#EFEFEF"><?php echo $row['gname']; ?></td>
-                            <td bgcolor="#EFEFEF"><?php echo $row['gender']; ?></td>
-                            <td bgcolor="#EFEFEF"><?php echo $row['dob']; ?></td>
-                            <td bgcolor="#EFEFEF"><?php echo $row['address']; ?></td>
-                             <td bgcolor="#EFEFEF"><?php echo $row['contact']; ?></td>
-                            <td bgcolor="#EFEFEF"><?php echo $row['email']; ?></td>
-                            <td bgcolor="#EFEFEF"><?php echo $row['ms']; ?></td>
-                            <td bgcolor="#EFEFEF"><?php echo $row['created']; ?></td>
-							<td bgcolor="#EFEFEF"><br>
-								<a href="edit.php?id=<?php echo $row['pid']; ?>">Edit</a>
-                                <a href="delete.php?id=<?php echo $row['pid']; ?>">Delete</a>
-                                <a href="print.php?id=<?php echo $row['pid']; ?>">Preview</a>
+                          <td bgcolor="#EFEFEF" height="46"><?php echo $row['fid']; ?></td>
+							<td bgcolor="#EFEFEF"><?php echo $row['rating']; ?></td>
+                            <td bgcolor="#EFEFEF"><?php echo $row['message']; ?></td>
+                           
 							</td>
 						</tr>
 						<?php
